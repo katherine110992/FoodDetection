1dd2b36d5db80d3e0ae2a7edb79b3b03dbc6f834
Katherine Espindola <yespindola@javeriana.edu.co>
2017-10-17 18:58:21 -0500

    flush missing

--- extraction/user_generated_content_batch.py
+++ extraction/user_generated_content_batch.py
@@ -185,6 +185,7 @@ def generate_food_detector_threads(day_start_date, day_finish_date, p_file):
     for thread in data_threads:
         for message in thread.messages:
             p_file.write(message)
+            p_file.flush()
         anagrams_with_food_words += thread.anagrams_with_food_words
         user_mentions_about_food += thread.user_mentions_about_food
         user_mentions_with_food_words += thread.user_mentions_with_food_words
